d7cb0d65e5a58046bbe9e1e89190d1ed084e6fec
Ryan Hafeman <ryanhafeman@marthafzsiphone.reshall>
2016-04-11 11:36:27 -0500

    initial commit

---
+++ Driver.java
@@ -0,0 +1,96 @@
+
+public class Driver 
+{
+	public static void main(String[] args)
+	{
+		String s = "hello";
+		String s2 = "elephant";
+		String s3 = "h0w a3e y0u?";
+		System.out.println(Driver.removeVowels(s));
+		System.out.println(Driver.removeConsonants(s2));
+		System.out.println(Driver.removeDigits(s3));		
+	}
+	
+	static String removeVowels(String s)
+	{
+		String answer = "";
+		String allVowels = "aeiou";
+		for(int i = 0; i < s.length(); i = i + 1)
+		{
+			if(allVowels.indexOf(s.charAt(i)) == -1)
+			{
+				answer = answer + s.charAt(i);
+			}
+		}
+		return answer;
+	}
+	
+	static String removeConsonants(String s)
+	{
+		String answer = "";
+		String allConsonants = "bcdfghjklmnpqrstvwxyz";
+		for(int i = 0; i < s.length(); i = i + 1)
+		{
+			if(allConsonants.indexOf(s.charAt(i)) == -1)
+			{
+				answer = answer + s.charAt(i);
+			}
+		}
+		return answer;	}
+	
+	static String removeDigits(String s)
+	{
+		String answer = "";
+		String allDigits = "0123456789";
+		for(int i = 0; i < s.length(); i = i + 1)
+		{
+			if(allDigits.indexOf(s.charAt(i)) == -1)
+			{
+				answer = answer + s.charAt(i);
+			}
+		}
+		return answer;
+	}
+		
+	static int indexOf(String s, char c)
+	{
+		for(int i = 0; i < s.length(); i = i + 1)
+		{
+			if(s.charAt(i) == c)
+			{
+				return i;
+			}
+		}
+		//if we are still kicking....
+		return -1;
+	}
+	
+	static void displayString(String s)
+	{
+		//should display the contents of the String one character per line
+		for(int i = 0; i < s.length(); i = i + 1)
+		{
+			System.out.println(s.charAt(i));
+		}
+	}
+	
+	static void displayWithSpaces(String s)
+	{
+		String answer = "";
+		for(int i = 0; i < s.length(); i = i + 1)
+		{
+			answer = answer + s.charAt(i) + " ";
+		}
+		System.out.println(answer);
+	}
+	
+	static String reverseString(String s)
+	{
+		String answer = "";
+		for(int i = s.length()-1; i >= 0; i = i - 1)
+		{
+			answer = answer + s.charAt(i);
+		}
+		return answer;
+	}
+}
